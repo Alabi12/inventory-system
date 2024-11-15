e8b11eb669684604269cc8cd6682be1f02662ef1
Robert O. Alabi <wolo22001@gmail.com>
2024-11-15 17:48:31 +0000

    add dashboard to include analysis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_15_140622) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_15_151908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_15_140622) do
     t.string "email"
     t.string "phone"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventory_items", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.integer "product_id"
+    t.integer "supplier_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
